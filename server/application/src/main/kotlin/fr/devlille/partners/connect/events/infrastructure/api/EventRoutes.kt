e68c6a300a580aeeb5319b20156a0a6871ba6907
GerardPaligot <gerard.paligot@gmail.com>
2025-08-02 15:06:35 +0200

    feat(server): grant automatically auth user to the created event

--- server/application/src/main/kotlin/fr/devlille/partners/connect/events/infrastructure/api/EventRoutes.kt
+++ server/application/src/main/kotlin/fr/devlille/partners/connect/events/infrastructure/api/EventRoutes.kt
@@ -1,8 +1,12 @@
 package fr.devlille.partners.connect.events.infrastructure.api
 
+import fr.devlille.partners.connect.auth.domain.AuthRepository
 import fr.devlille.partners.connect.events.domain.Event
 import fr.devlille.partners.connect.events.domain.EventRepository
+import fr.devlille.partners.connect.internal.infrastructure.api.UnauthorizedException
+import fr.devlille.partners.connect.internal.infrastructure.api.token
 import fr.devlille.partners.connect.internal.infrastructure.uuid.toUUID
+import fr.devlille.partners.connect.users.domain.UserRepository
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.request.receive
@@ -12,9 +16,12 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.put
 import org.koin.ktor.ext.inject
+import kotlin.getValue
 
 fun Route.eventRoutes() {
     val repository by inject<EventRepository>()
+    val authRepository by inject<AuthRepository>()
+    val userRepository by inject<UserRepository>()
 
     get {
         call.respond(repository.getAllEvents())
@@ -22,15 +29,22 @@ fun Route.eventRoutes() {
 
     post {
         val request = call.receive<Event>()
+        val token = call.token
         val id = repository.createEvent(request)
+        val userInfo = authRepository.getUserInfo(token)
+        userRepository.grantUsers(id, listOf(userInfo.email))
         call.respond(
             status = HttpStatusCode.Created,
             message = mapOf("id" to id.toString()),
         )
     }
 
-    put("/{id}") {
-        val id = call.parameters["id"]?.toUUID() ?: throw BadRequestException("Missing id")
+    put("/{eventId}") {
+        val id = call.parameters["eventId"]?.toUUID() ?: throw BadRequestException("Missing id")
+        val token = call.token
+        val userInfo = authRepository.getUserInfo(token)
+        val canEdit = userRepository.hasEditPermissionByEmail(userInfo.email, id)
+        if (!canEdit) throw UnauthorizedException("You are not allowed to edit this event")
         val request = call.receive<Event>()
         repository.updateEvent(id, request)
         call.respond(
