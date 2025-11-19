11c49898e23446f419814a52ec2e257a7a2b52bd
Gerard Paligot <gerard.paligot@gmail.com>
2025-11-19 22:17:13 +0100

    fix(server): add missing integrations endpoint in openapi

--- server/application/src/main/kotlin/fr/devlille/partners/connect/integrations/domain/IntegrationProvider.kt
+++ server/application/src/main/kotlin/fr/devlille/partners/connect/integrations/domain/IntegrationProvider.kt
@@ -1,10 +1,23 @@
 package fr.devlille.partners.connect.integrations.domain
 
+import kotlinx.serialization.SerialName
+
 enum class IntegrationProvider {
+    @SerialName("slack")
     SLACK,
+
+    @SerialName("mailjet")
     MAILJET,
+
+    @SerialName("qonto")
     QONTO,
+
+    @SerialName("billetweb")
     BILLETWEB,
+
+    @SerialName("webhook")
     WEBHOOK,
+
+    @SerialName("openplanner")
     OPENPLANNER,
 }
