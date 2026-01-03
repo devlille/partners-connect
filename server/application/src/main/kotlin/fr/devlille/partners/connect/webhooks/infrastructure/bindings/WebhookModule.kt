1739232e1d048f4be5893c2897d428dd782e6622
Gerard Paligot <gerard.paligot@gmail.com>
2026-01-03 23:11:11 +0100

    fix(server): use a custom http client for webhook integration

--- server/application/src/main/kotlin/fr/devlille/partners/connect/webhooks/infrastructure/bindings/WebhookModule.kt
+++ server/application/src/main/kotlin/fr/devlille/partners/connect/webhooks/infrastructure/bindings/WebhookModule.kt
@@ -1,10 +1,24 @@
 package fr.devlille.partners.connect.webhooks.infrastructure.bindings
 
+import fr.devlille.partners.connect.internal.infrastructure.bindings.getHttpClientEngine
+import fr.devlille.partners.connect.internal.infrastructure.bindings.networkEngineModule
 import fr.devlille.partners.connect.webhooks.application.WebhookRepositoryExposed
 import fr.devlille.partners.connect.webhooks.domain.WebhookRepository
 import fr.devlille.partners.connect.webhooks.infrastructure.gateways.HttpWebhookGateway
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val webhookModule = module {
-    single<WebhookRepository> { WebhookRepositoryExposed(listOf(HttpWebhookGateway(get()))) }
+    includes(networkEngineModule)
+    single<WebhookRepository> {
+        val client = HttpClient(getHttpClientEngine) {
+            install(ContentNegotiation) {
+                json(Json { ignoreUnknownKeys = true })
+            }
+        }
+        WebhookRepositoryExposed(listOf(HttpWebhookGateway(client)))
+    }
 }
