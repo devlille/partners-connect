b49e6f6744efb50b9b66904a25b5270831149e37
Emmanuel <demey.emmanuel@gmail.com>
2026-04-23 14:11:49 +0100

    fix: review proxy

--- front/server/routes/api-proxy/[...path].ts
+++ front/server/routes/api-proxy/[...path].ts
@@ -1,7 +1,8 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
+  const baseUrl = config.public.apiBaseUrl || 'http://localhost:8080';
   const path = getRouterParam(event, 'path');
-  const target = `${config.API_BASE_URL}/${path}`;
+  const target = `${baseUrl}/${path}`;
 
   return proxyRequest(event, target);
 });
