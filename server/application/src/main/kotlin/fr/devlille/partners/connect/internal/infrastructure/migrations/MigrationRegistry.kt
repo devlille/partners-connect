e8ff957495e850cb72479a83461f39e6826c4d16
Gerard Paligot <gerard.paligot@gmail.com>
2026-05-16 23:54:03 +0200

    feat(server): add ecosystem_partners table with curation state

--- server/application/src/main/kotlin/fr/devlille/partners/connect/internal/infrastructure/migrations/MigrationRegistry.kt
+++ server/application/src/main/kotlin/fr/devlille/partners/connect/internal/infrastructure/migrations/MigrationRegistry.kt
@@ -20,6 +20,7 @@ import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateCommunicationPlansTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateCompanyJobOffersTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateEcosystemPartnerCategoriesTableMigration
+import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateEcosystemPartnersTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateJobOfferPromotionsTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreatePartnershipSupportVideosTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateQandaTablesMigration
@@ -65,6 +66,7 @@ object MigrationRegistry {
         CreatePartnershipSupportVideosTableMigration,
         AddFlyerTemplateColumnsToSponsoringPacksMigration,
         CreateEcosystemPartnerCategoriesTableMigration,
+        CreateEcosystemPartnersTableMigration,
     )
 
     /**
