6739c4ae18d82a86c5d9ea5544a771f6029f0c8f
Gérard Paligot <gerard.paligot@gmail.com>
2026-05-21 22:29:28 +0200

    Revert "feat(server): add ecosystem partner domain DTOs and repository interfaces"
    This reverts commit dc2c131bc3a3aec541bb16a3a5b9315ced4b912d.

--- server/application/src/main/kotlin/fr/devlille/partners/connect/ecosystempartners/domain/EcosystemPartnerDecisionRepository.kt
+++
@@ -1,9 +0,0 @@
-package fr.devlille.partners.connect.ecosystempartners.domain
-
-import java.util.UUID
-
-interface EcosystemPartnerDecisionRepository {
-    fun validate(eventSlug: String, ecosystemPartnerId: UUID): UUID
-
-    fun decline(eventSlug: String, ecosystemPartnerId: UUID): UUID
-}
