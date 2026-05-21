af4f5fc9d6b83ae2ffab99cdc8ac47e6f8e029d4
Gérard Paligot <gerard.paligot@gmail.com>
2026-05-21 22:29:28 +0200

    Revert "feat(server): add ecosystem partner categories table"
    This reverts commit f2a2e0ad6eec890c0db674524ae3274496991a13.

--- server/application/src/main/kotlin/fr/devlille/partners/connect/internal/infrastructure/migrations/MigrationRegistry.kt
+++ server/application/src/main/kotlin/fr/devlille/partners/connect/internal/infrastructure/migrations/MigrationRegistry.kt
@@ -19,7 +19,6 @@ import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateBoothActivitiesTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateCommunicationPlansTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateCompanyJobOffersTableMigration
-import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateEcosystemPartnerCategoriesTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateJobOfferPromotionsTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreatePartnershipSupportVideosTableMigration
 import fr.devlille.partners.connect.internal.infrastructure.migrations.versions.CreateQandaTablesMigration
@@ -64,7 +63,6 @@ object MigrationRegistry {
         AddSpeakerSourceColumnMigration,
         CreatePartnershipSupportVideosTableMigration,
         AddFlyerTemplateColumnsToSponsoringPacksMigration,
-        CreateEcosystemPartnerCategoriesTableMigration,
     )
 
     /**
